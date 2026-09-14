432a40f03f1c11a0a84e770340108bb032f523c3
Arjun <arjunchipade656@gmail.com>
2026-09-14 15:25:22 +0530

    First commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+     Scanner pr = new Scanner(System.in);
+     int a = pr.nextInt();
+     int b = pr.nextInt();
+        if (a == b) {
+            System.out.println("equal");
+        } else if (a > b) {
+            System.out.println("a is greater");
+        } else {
+            System.out.println("a is lesser");
+        }
+        
+
+        }
+        }
+     
+
+    
+
